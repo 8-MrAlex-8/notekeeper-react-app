734524ded3adc6a1535cb54992fbdb63d62a255a
Lanz Alexander Malto <163155261+8-MrAlex-8@users.noreply.github.com>
2025-04-20 00:09:55 +0800

    [UPDATE] Added transition for 'Read Note' functionality.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,13 +7,27 @@ const cardStyle = {
 
 function Card(props) {
   return (
+    // The card itself
     <div
       className="bg-white rounded-md m-5 h-50 w-2/3 md:w-1/4 flex flex-col justify-between"
       style={cardStyle}
     >
-      <p className="px-6 pt-3 pb-0 overflow-hidden line-clamp-6 text-justify">
-        {props.content}
-      </p>
+      {/* White part of the card */}
+      <div
+        className="px-6 pt-3 pb-0 overflow-hidden text-justify group cursor-pointer
+      flex flex-col justify-center items-center"
+      >
+        {/*Notes Content*/}
+        <p className="line-clamp-6 transition group-hover:opacity-5">
+          {props.content}
+        </p>
+        {/*See Note prompt*/}
+        <p className="hidden group-hover:inline text-md absolute text-gray-700 tracking-wider">
+          Read Note
+        </p>
+      </div>
+
+      {/* Gray part of the card */}
       <CardUtility date={props.date} />
     </div>
   );
--- src/components/CardUtility.jsx
+++ src/components/CardUtility.jsx
@@ -3,8 +3,8 @@ import { Pencil, Trash, Copy } from "lucide-react";
 
 function CardUtility(props) {
   return (
-    <div className="flex justify-between items-center bg-gray-300 rounded-b-lg py-2 px-8">
-      <div className="block text-gray-600">
+    <div className="flex justify-between md:justify-center lg:justify-between items-center bg-gray-300 rounded-b-lg py-2 px-8">
+      <div className="block md:hidden lg:block text-gray-600">
         <p className="text-xs">{props.date}</p>
       </div>
 
